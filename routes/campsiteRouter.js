999ae289d9eafacb6c6dab42ef69a02e4b3a2a67
amad3usz <a.sepko@gmail.com>
2021-05-28 23:32:50 -0400

    Express Session Part 2

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -17,7 +17,7 @@ campsiteRouter
 	.post((req, res, next) => {
 		Campsite.create(req.body)
 			.then((campsite) => {
-				console.log('Campsite Created', campsite);
+				console.log('Campsite Created ', campsite);
 				res.statusCode = 200;
 				res.setHeader('Content-Type', 'application/json');
 				res.json(campsite);
@@ -59,9 +59,7 @@ campsiteRouter
 			{
 				$set: req.body,
 			},
-			{
-				new: true,
-			}
+			{ new: true }
 		)
 			.then((campsite) => {
 				res.statusCode = 200;
