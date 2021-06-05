0146315e1efd682cd14481f158b7d746be7ee03e
amad3usz <a.sepko@gmail.com>
2021-06-05 09:53:29 -0400

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
 	secretKey: '12345-67890-09876-54321',
 	mongoUrl: 'mongodb://localhost:27017/nucampsite',
+	facebook: {
+		clientId: '2930400833894009',
+		clientSecret: '0e226e57ff7f70a04b39337e0a348e49',
+	},
 };
